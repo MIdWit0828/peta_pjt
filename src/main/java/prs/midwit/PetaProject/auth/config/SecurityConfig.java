2d6099a027c4505e25c8a8f27fd09ea5f1abb29c
MIdWit0828 <kindj9897@gmail.com>
2024-10-06 14:56:54 +0900

    인증설정 및 파일 데이터 수정

--- src/main/java/prs/midwit/PetaProject/auth/config/SecurityConfig.java
+++ src/main/java/prs/midwit/PetaProject/auth/config/SecurityConfig.java
@@ -54,13 +54,15 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> {
                     /* 클라이언트가 외부 도메인을 요청하는 경우 웹 브라우저에서 자체적으로 사전 요청(preflight)이 일어난다.
                      * 이 때 OPTIONS 메소드로 서버에 사전 요청을 보내 확인한다. */
+                    auth.requestMatchers(HttpMethod.OPTIONS, "/api/v1/atts/**").authenticated();
+                    auth.requestMatchers(HttpMethod.OPTIONS, "/api/v1/atts").authenticated();
                     auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
-//                    auth.requestMatchers(HttpMethod.GET, "/productimgs/**").permitAll();
+                    //                    auth.requestMatchers(HttpMethod.GET, "/productimgs/**").permitAll();
 //                    auth.requestMatchers(HttpMethod.GET, "/api/v1/products/**").permitAll();
 //                    auth.requestMatchers(HttpMethod.POST, "/api/v1/members/signup", "/api/v1/members/login").permitAll();
 //                    auth.requestMatchers("/api/v1/products/*/reviews/**").authenticated();
 //                    auth.requestMatchers("/api/v1/products-management/**", "/api/v1/products/**").hasRole("ADMIN");
-                    auth.anyRequest().permitAll();
+//                    auth.anyRequest().permitAll();
                 })
                 /* 기본적으로 동작하는 로그인 필터 이전에 커스텀 로그인 필터를 설정한다. */
                 .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
